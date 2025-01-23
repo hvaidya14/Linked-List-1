4f160022d34591b1196afe8a9e143368e864dc53
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-22 22:01:54 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,53 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        if (head.next == null) {
+            return null;
+        }
+        boolean hasCycle = false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow){
+                hasCycle = true;
+                break;
+            }
+        }
+        if (!hasCycle) {
+            return null;
+        }
+        fast = head;
+        while (fast != null) {
+            System.out.println("loop2");
+            if (fast == slow){
+                break;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return fast;
+    }
+}
