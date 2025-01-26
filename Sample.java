f733dfc212fe9f5d20b7a4bf560d06a9c359acb8
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-26 11:07:08 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -91,3 +91,45 @@ class Solution {
         return;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int count =0;
+        if (head == null) {
+            return null;
+        }
+        ListNode curr = head;
+        while (curr != null) {
+            count++;
+            curr= curr.next;
+        }
+        int diff = count-n;
+        int i=0;
+        curr = head;
+        if (diff == 0) {
+            head=head.next;
+            return head;
+        }
+        while (i<diff-1) {
+            curr=curr.next;
+            i++;
+        }
+
+        if (curr.next != null) {
+            curr.next = curr.next.next;
+        }
+
+        return head;
+    }
+}
